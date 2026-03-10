2d0d6175cdc192ef09363f7999d443628883fc97
vasanthchakka <vasanthchakka1@gmail.com>
2026-03-10 15:15:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,67 @@
-function showSection(sectionId){
+const ctx1 = document.getElementById('incidentChart');
 
-    let sections = document.querySelectorAll(".section");
+new Chart(ctx1, {
 
-    sections.forEach(function(sec){
-        sec.style.display = "none";
-    });
+type: 'bar',
 
-    document.getElementById(sectionId).style.display = "block";
+data: {
+
+labels: ['Jan','Feb','Mar','Apr','May','Jun'],
+
+datasets: [
+
+{
+
+label:'Minor incidents',
+
+data:[5,2,4,1,6,2],
+
+backgroundColor:'#00d4ff'
+
+},
+
+{
+
+label:'Serious incidents',
+
+data:[1,0,1,0,2,0],
+
+backgroundColor:'#ffd166'
+
 }
+
+]
+
+}
+
+});
+
+
+
+const ctx2 = document.getElementById('evacuationChart');
+
+new Chart(ctx2, {
+
+type: 'line',
+
+data:{
+
+labels:['Apr','Jul','Oct','Jan','Apr'],
+
+datasets:[{
+
+label:'Evacuation time',
+
+data:[150,180,220,140,150],
+
+borderColor:'#00d4ff',
+
+fill:false,
+
+tension:0.4
+
+}]
+
+}
+
+});
